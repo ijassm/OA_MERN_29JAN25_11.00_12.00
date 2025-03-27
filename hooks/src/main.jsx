f5e3c24ef106485bb206a55693f8b2ec2156bf60
ijassm <ijass.ocean@gmail.com>
2025-03-27 12:18:55 +0530

    Add UseRef component to demonstrate useRef hook functionality

--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import Reducer from './Reducer.jsx'
-import UseEffect from './UseEffect'
+// import UseEffect from './UseEffect'
+import UseRef from './UseRef'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UseEffect />
+    <UseRef />
   </StrictMode>,
 )

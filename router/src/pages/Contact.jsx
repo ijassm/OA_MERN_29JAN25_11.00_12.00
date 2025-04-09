fca3e1b7effeaf023702df343a6ba6e982b054c8
ijassm <ijass.ocean@gmail.com>
2025-04-09 12:15:28 +0530

    feat: enhance routing and layout with Navbar, Error handling, and NotFound page

--- router/src/pages/Contact.jsx
+++ router/src/pages/Contact.jsx
@@ -1,8 +1,16 @@
+import { Navbar } from "../components/navbar";
+
 export const Contact = () => {
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>If you have any questions, feel free to reach out!</p>
-        </div>
+        <>
+            <Navbar />
+
+            <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+                <h1 className="text-4xl font-bold mb-4">Contact</h1>
+                <p className="text-lg">This is the contact page.</p>
+                <p className="text-lg">Have fun coding!</p>
+            </div>
+        </>
+
     );
 }
--- router/src/router.jsx
+++ router/src/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, Navigate } from "react-router"
 import { Home } from "./pages/Home.jsx"
 import { About } from "./pages/About.jsx";
 import { Contact } from "./pages/Contact.jsx";
+import { ErrorPage } from "./pages/ErrorPage.jsx";
+import { NotFoundPage } from "./pages/NotFoundPage.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,7 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
+        errorElement: <ErrorPage />,
         element: <Navigate to="home" />,
     },
     {
@@ -19,5 +22,9 @@ export const router = createBrowserRouter([
     {
         path: "contact",
         Component: Contact,
+    },
+    {
+        path: "*",
+        Component: NotFoundPage,
     },
 ]);
